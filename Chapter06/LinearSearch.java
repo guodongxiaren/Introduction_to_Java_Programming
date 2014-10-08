0c388a71db6482b1d8fd7adcd671359748c97bbf
guodongxiaren <879231132@qq.com>
2014-10-08 18:03:53 +0800

    big move

---
+++ Chapter06/LinearSearch.java
@@ -0,0 +1,10 @@
+public class LinearSearch {
+  /** The method for finding a key in the list */
+  public static int linearSearch(int[] list, int key) {
+    for (int i = 0; i < list.length; i++) {
+      if (key == list[i])
+        return i;
+    }
+    return -1;
+  }
+}
